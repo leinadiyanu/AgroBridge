10925f9c6cb09015963ba69790dd7ab25f560ec3
Daniel Akande <danielakande33@gmail.com>
2026-06-24 00:14:08 +0100

    added the cors to server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import {requestLogger, errorHandler, } from "./shared/middleware/index.js";
+import express from "express";
+import { requestLogger, errorHandler } from "./shared/middleware/index.js";
 import ussdRoutes from "./modules/ussd/routes.js";
 import authRoutes from "./modules/auth/routes.js";
 import usersRoutes from "./modules/users/routes.js";
@@ -8,12 +8,12 @@ import predictionRoutes from "./modules/predictions/routes.js";
 import orderRoutes from "./modules/orders/routes.js";
 import paymentRoutes from "./modules/payments/routes.js";
 import logisticsRoutes from "./modules/logistics/routes.js";
-import session from 'express-session';
-import { RedisStore } from 'connect-redis';
-import redisClient from './config/redis.js';
+import session from "express-session";
+import { RedisStore } from "connect-redis";
+import redisClient from "./config/redis.js";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger.js";
-
+import cors from "cors";
 
 const app = express();
 
@@ -23,26 +23,30 @@ app.use(requestLogger);
 app.use(
   session({
     store: new RedisStore({ client: redisClient }),
-    secret: process.env.SESSION_SECRET ?? 'your-secret-key',
+    secret: process.env.SESSION_SECRET ?? "your-secret-key",
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === 'production', // HTTPS only in prod
+      secure: process.env.NODE_ENV === "production", // HTTPS only in prod
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
-  })
+  }),
+);
+app.use(
+  cors({
+    origin: ["http://localhost:8081"],
+    credentials: true,
+  }),
 );
 
-app.get('/health', (req, res) => {
-  res.json({ message: 'AgroBridge API is running',
-    status: 'OK',
-   });
+app.get("/health", (req, res) => {
+  res.json({ message: "AgroBridge API is running", status: "OK" });
 });
 
-app.use('/auth', authRoutes);
-app.use('/users', usersRoutes);
-app.use('/ussd', ussdRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/ussd", ussdRoutes);
 app.use("/listings", listingRoutes);
 app.use("/predictions", predictionRoutes);
 app.use("/orders", orderRoutes);
